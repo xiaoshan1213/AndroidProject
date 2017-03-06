68c17b80b8aba9336c8001c833ee9b16f09cf963
Wenjin Ma <xiaoshan1213@gmail.com>
2017-03-05 23:26:17 -0800

    Game of Life
    android project of a small game.

---
+++ GameOfLife/src/main/java/com/cmpe277/sam/gameoflifedraw/MainActivity.java
@@ -0,0 +1,55 @@
+package com.cmpe277.sam.gameoflifedraw;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GameView gameview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+        gameview = (GameView) findViewById(R.id.gameview);
+    }
+
+    public void reset(View view){
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+        builder1.setMessage("are you sure to reset?");
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        gameview.reset();
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+    }
+
+    public void next(View view){
+        gameview.next();
+    }
+}
